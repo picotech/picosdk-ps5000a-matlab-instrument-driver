968aad633357850dddfe15093177c83a87afd7b3
hsmistry <hsmistry@users.noreply.github.com>
2019-10-24 14:42:18 +0100

    Update warning messages

--- ps5000aSetConfig.m
+++ ps5000aSetConfig.m
@@ -36,183 +36,185 @@ function [ps5000aStructs, ps5000aEnuminfo] = ps5000aSetConfig()
 %
 % Copyright: © 2019 Pico Technology Ltd. See LICENSE file for terms.	
 
-%% Set path to shared libraries, prototype and thunk Files
-% Set paths to shared library files, prototype and thunk files according to
-% the operating system and architecture.
+    %% Set path to shared libraries, prototype and thunk Files
+    % Set paths to shared library files, prototype and thunk files according to
+    % the operating system and architecture.
 
-% Identify working directory
-ps5000aConfigInfo.workingDir = pwd;
+    % Identify working directory
+    ps5000aConfigInfo.workingDir = pwd;
 
-% Find file name
-ps5000aConfigInfo.configFileName = mfilename('fullpath');
+    % Find file name
+    ps5000aConfigInfo.configFileName = mfilename('fullpath');
 
-% Only require the path to the config file
-[ps5000aConfigInfo.pathStr] = fileparts(ps5000aConfigInfo.configFileName);
+    % Only require the path to the config file
+    [ps5000aConfigInfo.pathStr] = fileparts(ps5000aConfigInfo.configFileName);
 
-% Identify architecture e.g. 'win64'
-ps5000aConfigInfo.archStr = computer('arch');
-ps5000aConfigInfo.archPath = fullfile(ps5000aConfigInfo.pathStr, ps5000aConfigInfo.archStr);
+    % Identify architecture e.g. 'win64'
+    ps5000aConfigInfo.archStr = computer('arch');
+    ps5000aConfigInfo.archPath = fullfile(ps5000aConfigInfo.pathStr, ps5000aConfigInfo.archStr);
 
-% Add path to Prototype and Thunk files if not already present
-if (isempty(strfind(path, ps5000aConfigInfo.archPath)))
-    
-    try
+    % Add path to Prototype and Thunk files if not already present
+    if (isempty(strfind(path, ps5000aConfigInfo.archPath)))
 
-        addpath(ps5000aConfigInfo.archPath);
+        try
 
-    catch err
+            addpath(ps5000aConfigInfo.archPath);
 
-        error('PS5000aConfig:OperatingSystemNotSupported', 'Operating system not supported - please contact support@picotech.com');
+        catch err
+
+            error('ps5000aSetConfig:OperatingSystemNotSupported', 'Operating system not supported - please contact support@picotech.com');
+
+        end
 
     end
-    
-end
 
-% Set the path to shared libraries according to operating system.
+    % Set the path to shared libraries according to operating system.
 
-% Define possible paths for drivers - edit to specify location of drivers
+    % Define possible paths for drivers - edit to specify location of drivers
 
-ps5000aConfigInfo.macDriverPath = '/Applications/PicoScope6.app/Contents/Resources/lib';
-ps5000aConfigInfo.linuxDriverPath = '/opt/picoscope/lib/';
+    ps5000aConfigInfo.macDriverPath = '/Applications/PicoScope6.app/Contents/Resources/lib';
+    ps5000aConfigInfo.linuxDriverPath = '/opt/picoscope/lib/';
 
-ps5000aConfigInfo.winSDKInstallPath = 'C:\Program Files\Pico Technology\SDK';
-ps5000aConfigInfo.winDriverPath = fullfile(ps5000aConfigInfo.winSDKInstallPath, 'lib');
+    ps5000aConfigInfo.winSDKInstallPath = 'C:\Program Files\Pico Technology\SDK';
+    ps5000aConfigInfo.winDriverPath = fullfile(ps5000aConfigInfo.winSDKInstallPath, 'lib');
 
-%32-bit version of MATLAB on Windows 64-bit
-ps5000aConfigInfo.woW64SDKInstallPath = 'C:\Program Files (x86)\Pico Technology\SDK'; 
-ps5000aConfigInfo.woW64DriverPath = fullfile(ps5000aConfigInfo.woW64SDKInstallPath, 'lib');
+    %32-bit version of MATLAB on Windows 64-bit
+    ps5000aConfigInfo.woW64SDKInstallPath = 'C:\Program Files (x86)\Pico Technology\SDK'; 
+    ps5000aConfigInfo.woW64DriverPath = fullfile(ps5000aConfigInfo.woW64SDKInstallPath, 'lib');
 
-if (ismac())
-    
-    % Libraries (including wrapper libraries) are stored in the PicoScope
-    % 6 App folder. Add locations of library files to environment variable.
-    
-    setenv('DYLD_LIBRARY_PATH', ps5000aConfigInfo.macDriverPath);
-    
-    if(contains(getenv('DYLD_LIBRARY_PATH'), ps5000aConfigInfo.macDriverPath))
-       
-        addpath(ps5000aConfigInfo.macDriverPath);
-        
-    else
-        
-        warning('PS5000aConfig:LibraryPathNotFound','Locations of libraries not found in DYLD_LIBRARY_PATH');
-        
-    end
-    
-elseif (isunix())
-	    
-    % Add path to drivers if not already on the MATLAB path
-    if (isempty(strfind(path, ps5000aConfigInfo.linuxDriverPath)))
-        
-        addpath(ps5000aConfigInfo.linuxDriverPath);
-            
-    end
-		
-elseif (ispc())
-    
-    % Microsoft Windows operating system
-    
-    % Set path to dll files if the Pico Technology PicoSDK installer has been
-    % used or place dll files in the folder corresponding to the
-    % architecture. Detect if 32-bit version of MATLAB on 64-bit Microsoft
-    % Windows.
-    
-    ps5000aConfigInfo.winSDKInstallPath = '';
-    
-    if (strcmp(ps5000aConfigInfo.archStr, 'win32') && exist('C:\Program Files (x86)\', 'dir') == 7)
-        
+    if (ismac())
+
+        % Libraries (including wrapper libraries) are stored in the PicoScope
+        % 6 App folder. Add locations of library files to environment variable.
+
+        setenv('DYLD_LIBRARY_PATH', ps5000aConfigInfo.macDriverPath);
+
+        if(contains(getenv('DYLD_LIBRARY_PATH'), ps5000aConfigInfo.macDriverPath))
+
+            addpath(ps5000aConfigInfo.macDriverPath);
+
+        else
+
+            warning('ps5000aSetConfig:LibraryPathNotFound','Locations of libraries not found in DYLD_LIBRARY_PATH');
+
+        end
+
+    elseif (isunix())
+
         % Add path to drivers if not already on the MATLAB path
-        if (isempty(strfind(path, ps5000aConfigInfo.woW64DriverPath)))
-            
-            try 
+        if (isempty(strfind(path, ps5000aConfigInfo.linuxDriverPath)))
 
-                addpath(ps5000aConfigInfo.woW64DriverPath);
+            addpath(ps5000aConfigInfo.linuxDriverPath);
 
-            catch err
+        end
 
-                warning('PS5000aConfig:DirectoryNotFound', ['Folder C:\Program Files (x86)\Pico Technology\SDK\lib\ not found. '...
-                    'Please ensure that the location of the library files are on the MATLAB path.']);
+    elseif (ispc())
+
+        % Microsoft Windows operating system
+
+        % Set path to dll files if the Pico Technology PicoSDK installer has been
+        % used or place dll files in the folder corresponding to the
+        % architecture. Detect if 32-bit version of MATLAB on 64-bit Microsoft
+        % Windows.
+
+        ps5000aConfigInfo.winSDKInstallPath = '';
+
+        if (strcmp(ps5000aConfigInfo.archStr, 'win32') && exist('C:\Program Files (x86)\', 'dir') == 7)
+
+            % Add path to drivers if not already on the MATLAB path
+            if (isempty(strfind(path, ps5000aConfigInfo.woW64DriverPath)))
+
+                try 
+
+                    addpath(ps5000aConfigInfo.woW64DriverPath);
+
+                catch err
+
+                    warning('ps5000aSetConfig:DirectoryNotFound', ['Folder C:\Program Files (x86)\Pico Technology\SDK\lib\ not found. '...
+                        'Please ensure that the location of the library files are on the MATLAB path.']);
+
+                end
 
             end
-            
-        end
-        
-    else
-        
-        % 32-bit MATLAB on 32-bit Windows or 64-bit MATLAB on 64-bit
-        % Windows operating systems
-        
-        % Add path to drivers if not already on the MATLAB path
-        if (isempty(strfind(path, ps5000aConfigInfo.winDriverPath)))
-            
-            try 
 
-                addpath(ps5000aConfigInfo.winDriverPath);
+        else
 
-            catch err
+            % 32-bit MATLAB on 32-bit Windows or 64-bit MATLAB on 64-bit
+            % Windows operating systems
 
-                warning('PS5000aConfig:DirectoryNotFound', ['Folder C:\Program Files\Pico Technology\SDK\lib\ not found. '...
-                    'Please ensure that the location of the library files are on the MATLAB path.']);
+            % Add path to drivers if not already on the MATLAB path
+            if (isempty(strfind(path, ps5000aConfigInfo.winDriverPath)))
+
+                try 
+
+                    addpath(ps5000aConfigInfo.winDriverPath);
+
+                catch err
+
+                    warning('ps5000aSetConfig:DirectoryNotFound', ['Folder C:\Program Files\Pico Technology\SDK\lib\ not found. '...
+                        'Please ensure that the location of the library files are on the MATLAB path.']);
+
+                end
 
             end
-            
+
         end
-        
+
+    else
+
+        error('ps5000aSetConfig:OperatingSystemNotSupported', 'Operating system not supported - please contact support@picotech.com');
+
     end
-    
-else
-    
-    error('PS5000aConfig:OperatingSystemNotSupported', 'Operating system not supported - please contact support@picotech.com');
+
+    %% Set path for PicoScope Support Toolbox files if not installed
+    % Set MATLAB Path to include location of PicoScope Support Toolbox
+    % Functions and Classes if the Toolbox has not been installed. Installation
+    % of the toolbox is only supported in MATLAB 2014b and later versions.
+
+    % Check if PicoScope Support Toolbox is installed - using code based on
+    % <http://stackoverflow.com/questions/6926021/how-to-check-if-matlab-toolbox-installed-in-matlab How to check if matlab toolbox installed in matlab>
+
+    ps5000aConfigInfo.psTbxName = 'PicoScope Support Toolbox';
+    ps5000aConfigInfo.v = ver; % Find installed toolbox information
+
+    if (~any(strcmp(ps5000aConfigInfo.psTbxName, {ps5000aConfigInfo.v.Name})))
+
+        warning('ps5000aSetConfig:PSTbxNotFound', 'PicoScope Support Toolbox not found, searching for folder.');
+
+        % If the PicoScope Support Toolbox has not been installed, check to see
+        % if the folder is on the MATLAB path, having been downloaded via zip
+        % file.
+
+        ps5000aConfigInfo.psTbxFound = strfind(path, ps5000aConfigInfo.psTbxName);
+
+        if (isempty(ps5000aConfigInfo.psTbxFound))
+
+            ps5000aConfigInfo.psTbxNotFoundWarningMsg = sprintf(['Please either:\n'...
+                '(1) install the PicoScope Support Toolbox via the Add-Ons Explorer or\n'...
+                '(2) download the zip file from MATLAB Central File Exchange and add the location of the extracted contents to the MATLAB path.']);
+
+            warning('ps5000aSetConfig:PSTbxDirNotFound', ['PicoScope Support Toolbox not found. ', ps5000aConfigInfo.psTbxNotFoundWarningMsg]);
+
+            ps5000aConfigInfo.f = warndlg(ps5000aConfigInfo.psTbxNotFoundWarningMsg, 'PicoScope Support Toolbox Not Found', 'modal');
+            uiwait(ps5000aConfigInfo.f);
+
+            web('https://uk.mathworks.com/matlabcentral/fileexchange/53681-picoscope-support-toolbox');
+
+        end
+
+    end
+
+    % Change back to the folder where the script was called from.
+    cd(ps5000aConfigInfo.workingDir);
+
+    %% Load enumerations and structure information
+    % Enumerations and structures are used by certain Intrument Driver functions.
+
+    % Find prototype file names based on architecture
+
+    ps5000aConfigInfo.ps5000aMFile = str2func(strcat('ps5000aMFile_', ps5000aConfigInfo.archStr));
+    ps5000aConfigInfo.ps5000aWrapMFile = str2func(strcat('ps5000aWrapMFile_', ps5000aConfigInfo.archStr));
+
+    [~, ps5000aStructs, ps5000aEnuminfo, ~] = ps5000aConfigInfo.ps5000aMFile(); 
     
 end
-
-%% Set path for PicoScope Support Toolbox files if not installed
-% Set MATLAB Path to include location of PicoScope Support Toolbox
-% Functions and Classes if the Toolbox has not been installed. Installation
-% of the toolbox is only supported in MATLAB 2014b and later versions.
-
-% Check if PicoScope Support Toolbox is installed - using code based on
-% <http://stackoverflow.com/questions/6926021/how-to-check-if-matlab-toolbox-installed-in-matlab How to check if matlab toolbox installed in matlab>
-
-ps5000aConfigInfo.psTbxName = 'PicoScope Support Toolbox';
-ps5000aConfigInfo.v = ver; % Find installed toolbox information
-
-if (~any(strcmp(ps5000aConfigInfo.psTbxName, {ps5000aConfigInfo.v.Name})))
-   
-    warning('PS5000aConfig:PSTbxNotFound', 'PicoScope Support Toolbox not found, searching for folder.');
-    
-    % If the PicoScope Support Toolbox has not been installed, check to see
-    % if the folder is on the MATLAB path, having been downloaded via zip
-    % file.
-    
-    ps5000aConfigInfo.psTbxFound = strfind(path, ps5000aConfigInfo.psTbxName);
-    
-    if (isempty(ps5000aConfigInfo.psTbxFound))
-        
-        ps5000aConfigInfo.psTbxNotFoundWarningMsg = sprintf(['Please either:\n'...
-            '(1) install the PicoScope Support Toolbox via the Add-Ons Explorer or\n'...
-            '(2) download the zip file from MATLAB Central File Exchange and add the location of the extracted contents to the MATLAB path.']);
-        
-        warning('PS5000aConfig:PSTbxDirNotFound', ['PicoScope Support Toolbox not found. ', ps5000aConfigInfo.psTbxNotFoundWarningMsg]);
-        
-        ps5000aConfigInfo.f = warndlg(ps5000aConfigInfo.psTbxNotFoundWarningMsg, 'PicoScope Support Toolbox Not Found', 'modal');
-        uiwait(ps5000aConfigInfo.f);
-        
-        web('https://uk.mathworks.com/matlabcentral/fileexchange/53681-picoscope-support-toolbox');
-            
-    end
-    
-end
-
-% Change back to the folder where the script was called from.
-cd(ps5000aConfigInfo.workingDir);
-
-%% Load enumerations and structure information
-% Enumerations and structures are used by certain Intrument Driver functions.
-
-% Find prototype file names based on architecture
-
-ps5000aConfigInfo.ps5000aMFile = str2func(strcat('ps5000aMFile_', ps5000aConfigInfo.archStr));
-ps5000aConfigInfo.ps5000aWrapMFile = str2func(strcat('ps5000aWrapMFile_', ps5000aConfigInfo.archStr));
-
-[~, ps5000aStructs, ps5000aEnuminfo, ~] = ps5000aConfigInfo.ps5000aMFile(); 
